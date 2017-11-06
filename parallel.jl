f94d3471444448971ece5057e337663771a7c1bf
Madeleine Udell <madeleine.udell@gmail.com>
2017-11-06 13:57:25 -0500

    updates for julia v0.6

--- parallel.jl
+++ parallel.jl
@@ -16,7 +16,6 @@ Threads.nthreads()
 
 # simple example
 a = zeros(10)
-
 Threads.@threads for i = 1:10
     a[i] = Threads.threadid()
 end
@@ -32,5 +31,20 @@ end
 
 a = zeros(n)
 @time for i = 1:niters
+    a[i%n+1] += 1
+end
+
+
+n=10
+niters=4000000
+a = zeros(n)
+# if all works correctly, it will be faster than single threaded
+# note: += is not atomic here
+@time Threads.@threads for i = 1:niters
+    a[i%n+1] += 1
+end
+
+a = zeros(n)
+@parallel (+) for i = 1:niters
     a[i%n+1] += 1
 end
